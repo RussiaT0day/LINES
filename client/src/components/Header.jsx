fc05a8b4d77ac94a94ea59e377b0f849e61f98fb
ShuynovVA <ShuynovVA@suek.ru>
2022-07-09 08:14:07 +0300

    feat: Add component header in App

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import { PageHeader } from "antd";
+import React from "react";
+
+const Header = () => (
+			<>
+					<PageHeader
+							className="site-page-header"
+							title="Мой пет проект на React "
+					/>
+			</>
+
+);
+
+export default Header;
