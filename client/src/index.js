e5da7c0aea1d3e66ef3aba66f7ed8833c9b48e1f
ShuynovVA <ShuynovVA@suek.ru>
2022-07-09 08:13:28 +0300

    feat: Add antd disign in main

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import App from './App';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import 'antd/dist/antd.css';
+import 'antd/dist/antd.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
